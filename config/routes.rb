ea523bb09d86a81a608b8e81fa0a5ece0f1c6727
Joseph Valencia <jvalencia@ucla.edu>
2023-03-20 17:11:07 -0700

    added bootstrap from new rails project

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'static_pages#landing_page'
+end
