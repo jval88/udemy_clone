5f5b725b0290e0956c91e4082cc460279d849de1
Joseph Valencia <jvalencia@ucla.edu>
2023-05-25 17:15:57 -0700

    Allow homepage to be viewed without login. Add card for new/edit course view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:landing_page]
   def landing_page
     @courses = Course.all.limit(3)
     @recent_courses = Course.all.limit(3).order(created_at: :desc)
