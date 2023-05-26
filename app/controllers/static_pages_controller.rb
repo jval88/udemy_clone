3f082768598736efec209adbd3b463f346148d79
Joseph Valencia <jvalencia@ucla.edu>
2023-05-25 17:04:34 -0700

    Added new homepage view. Updated display for courses to have columns

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def landing_page
+    @courses = Course.all.limit(3)
+    @recent_courses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy
